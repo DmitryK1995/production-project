319ea3c65b21bfbb35c3686b12f0b38c03eca5fe
DmitryK1995 <KolesnikovD-S@yandex.ru>
2024-02-25 15:33:28 +0300

    add new theme, add react refresh plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
@@ -25,6 +26,7 @@ export function buildPlugins({
     ];
 
     if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin());
         plugins.push(new webpack.HotModuleReplacementPlugin());
         plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false,
