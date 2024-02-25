4f8af242d9dee6a3d6fe1bd143db2117be466569
DmitryK1995 <KolesnikovD-S@yandex.ru>
2024-02-25 22:28:42 +0300

    add atricles page

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,4 +1,6 @@
 import { AboutPage } from 'pages/AboutPage';
+import { ArticleDetailsPage } from 'pages/ArticleDetailsPage';
+import { ArticlesPage } from 'pages/ArticlesPage';
 import { MainPage } from 'pages/MainPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import { ProfilePage } from 'pages/ProfilePage';
@@ -12,6 +14,8 @@ export enum AppRouter {
     MAIN = 'main',
     ABOUT = 'about',
     PROFILE = 'profile',
+    ARTICLES = 'articles',
+    ARTICLE_DETAILS = 'article_details',
     // last
     NOT_FOUND = 'not_found',
 }
@@ -20,6 +24,8 @@ export const RoutePath: Record<AppRouter, string> = {
     [AppRouter.MAIN]: '/',
     [AppRouter.ABOUT]: '/about',
     [AppRouter.PROFILE]: '/profile',
+    [AppRouter.ARTICLES]: '/articles',
+    [AppRouter.ARTICLE_DETAILS]: '/articles/', // + :id
     [AppRouter.NOT_FOUND]: '*',
 };
 
@@ -35,6 +41,16 @@ export const routeConfig: Record<AppRouter, AppRoutesProps> = {
     [AppRouter.PROFILE]: {
         path: RoutePath.profile,
         element: <ProfilePage />,
+        authOnly: true,
+    },
+    [AppRouter.ARTICLES]: {
+        path: RoutePath.articles,
+        element: <ArticlesPage />,
+        authOnly: true,
+    },
+    [AppRouter.ARTICLE_DETAILS]: {
+        path: `${RoutePath.article_details}:id`,
+        element: <ArticleDetailsPage />,
         authOnly: true,
     },
 
