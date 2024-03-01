e642a2c921734f4f910dc718425483933fb76e71
DmitryK1995 <KolesnikovD-S@yandex.ru>
2024-03-01 13:02:50 +0300

    add skeleton, code, article details card

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -7,6 +7,7 @@ import { CounterSchema } from 'entities/Counter';
 import { ProfileSchema } from 'entities/Profile';
 import { UserSchema } from 'entities/User';
 import { LoginSchema } from 'features/AuthByUsername';
+import { ArticleDetailsSchema } from 'entities/Article';
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -15,6 +16,7 @@ export interface StateSchema {
     // асинхронные редюсеры
     loginForm?: LoginSchema;
     profile?: ProfileSchema;
+    articleDetails?: ArticleDetailsSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
---
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Article, ArticleBlockType, ArticleType } from 'entities/Article/model/types/article';
+import { ArticleDetails } from './ArticleDetails';
+
+export default {
+    title: 'entities/ArticleDetails',
+    component: ArticleDetails,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ArticleDetails>;
+
+const Template: ComponentStory<typeof ArticleDetails> = (args) => <ArticleDetails {...args} />;
+
+const article: Article = {
+    id: '1',
+    title: 'Javascript news',
+    subtitle: 'Что нового в JS за 2022?',
+    img: 'https://www.ankitweblogic.com/javascript/js_img/javascript.png',
+    views: 1022,
+    createdAt: '26.02.2024',
+    type: [ArticleType.IT],
+    blocks: [
+        {
+            id: '1',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка',
+                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js.',
+                'Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
+            ],
+        },
+        {
+            id: '4',
+            type: ArticleBlockType.CODE,
+            code: "<!DOCTYPE html>\n<html>\n<body>\n<p id='hello'></p>\n<script>\ndocument.getElementById('hello').innerHTML = 'Hello, world!'; \n</script>\n</body>\n</html>",
+        },
+        {
+            id: '5',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                'Если всё сделано правильно — под этой строчкой появится текст Hello, world!. На всё остальное пока не обращайте внимания.',
+            ],
+        },
+        {
+            id: '2',
+            type: ArticleBlockType.IMAGE,
+            src: 'https://www.thoughtco.com/thmb/094YF3bQBiuV_13n92GFQZWkM7k=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/JavaScript-58acbb8a3df78c345bad32c2.jpg',
+            title: 'Рисунок 1 - скриншот сайта',
+        },
+        {
+            id: '7',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js.',
+                'Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
+            ],
+        },
+        {
+            id: '8',
+            type: ArticleBlockType.IMAGE,
+            src: 'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709164800&semt=sph',
+            title: 'Рисунок 1 - скриншот сайта',
+        },
+        {
+            id: '9',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js.',
+            ],
+        },
+    ],
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
+Primary.decorators = [StoreDecorator({
+    articleDetails: {
+        data: article,
+    },
+})];
+
+export const Loading = Template.bind({});
+Loading.args = {
+};
+Loading.decorators = [StoreDecorator({
+    articleDetails: {
+        isLoading: true,
+    },
+})];
+
+export const Error = Template.bind({});
+Error.args = {
+};
+Error.decorators = [StoreDecorator({
+    articleDetails: {
+        error: 'error',
+    },
+})];
