3ed462f4aaecae11c540bba879e4d1308926cbaf
DmitryK1995 <KolesnikovD-S@yandex.ru>
2024-04-06 21:16:13 +0300

    add headless ui, add listbox component

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ListBox } from 'shared/ui/ListBox/ListBox';
-import { HStack } from 'shared/ui/Stack';
 import { Page } from 'widgets/Page/Page';
 
 const MainPage = () => {
@@ -16,25 +14,6 @@ const MainPage = () => {
     return (
         <Page>
             {t('Главная страница')}
-            <div>sdfsdf</div>
-            <HStack>
-                <div>fsdfsd</div>
-                <ListBox
-                    defaultValue="Выберите значение"
-                    onChange={(value: string) => {}}
-                    value={undefined}
-                    items={
-                        [
-                            { value: '1', content: '123' },
-                            { value: '12', content: 'asda', disabled: true },
-                            { value: '13', content: '1asdasd23' },
-                        ]
-                    }
-                />
-            </HStack>
-            <div>dfhhdfgh</div>
-            <div>dfhhdfgh</div>
-            <div>dfhhdfgh</div>
         </Page>
     );
 };
